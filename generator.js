82d1f43bf83228fc9948fe3fa3a60ee9d314dcd7
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2019-10-22 18:48:48 +0300

    web-interface

--- generator.js
+++ generator.js
@@ -1,9 +1,198 @@
-var code = "";
+let inLoop = false;
+let sampleSelected = false;
+let pauseState = false;
+let beatAdded = false;
 
-function startToneLibrary() {
-    code += "var synth = new Tone.Synth().toMaster();\n";
+let code = "";
+let beatCode = "";
+let octave = 4;
+let selectedSound = "piano";
+let selectedWave = "sine";
+let duration = "4n";
+let notes = [];
+
+let synth_code = "var synth = new Tone.Synth("+
+    "{ 'oscillator' : { 'type' : '{0}' }, " +
+    "'envelope' : { 'attack' : 0.1, 'decay': 0.1, 'sustain': 0.9, 'release': 1 } }"
+    +").toMaster();\n" +
+    "function playInterval(notes) { \n" +
+    "   var interval = new Tone.Sequence(function(time, note){ \n" +
+    "       synth.triggerAttackRelease(note.note, note.duration, time); \n" +
+    "   }, notes, '4n'); \n" +
+    "   interval.loop = {1};\n" +
+    "   interval.start(0);\n " +
+    "   Tone.Transport.start('+0.2');\n" +
+    "}\n" +
+    "function triggerSynth(time, note){ \n" +
+    "   synth.triggerAttackRelease(note.note, note.duration, time); \n" +
+    "} \n" +
+    "playInterval([{2}]);\n";
+
+let scheduledPlay = "function playInterval(notes) { \n " +
+    "   Tone.Transport.stop();\n" +
+    "   Tone.Transport.position = 0;\n" +
+    "   Tone.Transport.cancel();\n" +
+    "   Tone.Transport.schedule((time) => {\n" +
+    "       let relativeTime = 0;\n" +
+    "       for (const note of notes) {\n" +
+    "           const duration = note.dur;\n" +
+    "           synth.triggerAttackRelease(note.note, note.dur, time + relativeTime);\n" +
+    "           relativeTime += Tone.Time(duration).toSeconds();\n" +
+    "       }\n" +
+    "   }, '4m');\n" +
+    "   Tone.Transport.bpm.value = 120;\n" +
+    "   Tone.Transport.start();\n" +
+    "}" +
+    "playInterval([{note : 'A3', dur : '4n'}, {note : 'B4', dur : '16n'}]);";
+
+let sample_code = "var instruments = SampleLibrary.load({instruments: ['{0}']});\n" +
+    "Tone.Buffer.on('load', function () { \n" +
+    "   instruments['{1}'].toMaster(); \n" +
+    "   var interval = new Tone.Sequence(function (time, note) { \n" +
+    "       instruments['{2}'].triggerAttackRelease(note.note, note.duration, time); \n" +
+    "   }, {3}, '4n'); \n" +
+    "   interval.loop = {4}; \n" +
+    "   interval.start(0); \n" +
+    "   Tone.Transport.start('+0.2'); \n" +
+    "});";
+
+let add_beat = "var kick = new Tone.MembraneSynth();\n" +
+    "var kickdistortion = new Tone.Distortion(8);\n" +
+    "var kickdelay = new Tone.PingPongDelay({\n" +
+    "  'delayTime' : '8n',\n" +
+    "  'feedback' : 0.3,\n" +
+    "  'wet' : 0.5\n" +
+    "});\n" +
+    "var kickphaser = new Tone.Phaser();\n" +
+    "kick.chain(kickdistortion, kickdelay, kickphaser, Tone.Master);\n" +
+    "\n" +
+    "var kickloop = new Tone.Loop(function(time) {\n" +
+    "  kick.triggerAttackRelease('C1', '8n', time);\n" +
+    "}, '4n').start();";
+
+let s_code = "fnPlaySong([ ['E,0', 8], ['D,0', 8], ['C,0', 2], ['C,0', 8], ['D,0', 8], ['C,0', 8], ['E,0', 8], ['D,0', 1], ['C,0', 8], ['D,0', 8], ['E,0', 2], ['A,0', 8], ['G,0', 8], ['E,0', 8], ['C,0', 8], ['D,0', 1], ['A,0', 8], ['B,0', 8], ['C,1', 2], ['B,0', 8], ['C,1', 8], ['D,1', 8], ['C,1', 8], ['A,0', 1], ['G,0', 8], ['A,0', 8], ['B,0', 2], ['C,1', 8], ['B,0', 8], ['A,0', 8], ['G,0', 8], ['A,0', 1] ]);";
+
+let change_bpm = "Tone.Transport.bpm.value = {0}";
+
+let pause_play = "Tone.Transport.pause();\n";
+
+function getCode() {
+    code = "";
+    if (sampleSelected) {
+        code += sample_code.format(selectedSound, selectedSound, selectedSound, notes, inLoop);
+    }else{
+        code += synth_code.format(selectedWave, inLoop, notes);
+    }
+    if (beatAdded){
+        code += beatCode;
+    }
+    return code;
 }
 
-function playMidC() {
-    code += "synth.triggerAttackRelease('C4', '8n');\n";
+function resume(){
+    pauseState = false;
+    return getCode();
+}
+
+function pause(){
+    pauseState = true;
+    return pause_play;
+}
+
+function isPaused(){
+    return pauseState;
+}
+
+function changeOctave(oct) {
+    octave = oct;
+}
+
+function isSampleSelected() {
+    return sampleSelected;
+}
+
+function changeBPM(bpm){
+    return change_bpm.format(bpm);
+}
+
+
+function startSynth() {
+}
+
+function clearCode() {
+    code = "";
+    beatCode = "";
+    notes = [];
+}
+
+function deleteLastPart() {
+    if (code.length() > 0) {
+        let lastLineIndex = code.lastIndexOf('\n');
+        code = code.substring(0, lastLineIndex);
+    }
+}
+
+function startLoop() {
+    inLoop = true;
+}
+
+function addBeat(){
+    beatCode = "";
+    beatAdded = true;
+    beatCode += add_beat;
+}
+
+function deleteBeat(){
+    beatAdded = false;
+    beatCode = "";
+}
+
+function shortNote(){
+    duration = "8n";
+}
+
+
+function longNote(){
+    duration = "2n";
+}
+
+function selectSample(sample) {
+    selectedSound = sample;
+    sampleSelected = true;
+}
+
+function selectSynthWave(wave){
+    selectedWave = wave;
+    sampleSelected = false;
+}
+
+function addNote(note) {
+    switch (note) {
+        case 'A':
+            notes.push("{ time : {0}, note : 'A{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'B':
+            notes.push("{ time : {0}, note : 'B{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'C':
+            notes.push("{ time : {0}, note : 'C{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'D':
+            notes.push("{ time : {0}, note : 'D{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'E':
+            notes.push("{ time : {0}, note : 'E{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'F':
+            notes.push("{ time : {0}, note : 'F{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'G':
+            notes.push("{ time : {0}, note : 'G{1}', dur : '{2}'}".format( notes.length * 0.5, octave, duration));
+            break;
+        case 'N':
+            notes.push("null");
+            break;
+        default:
+            break;
+    }
 }
---
+++ libraries/tone/Tonejs-Instruments.js
@@ -0,0 +1,196 @@
+/**
+* @fileoverview A sample library and quick-loader for tone.js
+* 
+* @author N.P. Brosowsky (nbrosowsky@gmail.com)
+* https://github.com/nbrosowsky/tonejs-instruments
+*/
+
+var SampleLibrary = {
+    minify: false,
+    ext: '.[mp3|ogg]', // use setExt to change the extensions on all files // do not change this variable //
+    baseUrl: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/',
+    list: ['harmonium','piano','cello'],
+    onload: null,
+  
+    setExt: function (newExt) {
+        var i
+        for (i = 0; i <= this.list.length - 1; i++) {
+            for (var property in this[this.list[i]]) {
+
+                this[this.list[i]][property] = this[this.list[i]][property].replace(this.ext, newExt)
+            }
+
+
+        }
+        this.ext = newExt;
+        return console.log("sample extensions set to " + this.ext)
+    },
+
+    load: function (arg) {
+        var t, rt, i;
+        (arg) ? t = arg: t = {};
+        t.instruments = t.instruments || this.list;
+        t.baseUrl = t.baseUrl || this.baseUrl;
+        t.onload = t.onload || this.onload;
+
+        // update extensions if arg given
+        if (t.ext) {
+            if (t.ext != this.ext) {
+                this.setExt(t.ext)
+            }
+            t.ext = this.ext
+        }
+
+        rt = {};
+
+        // if an array of instruments is passed...
+        if (Array.isArray(t.instruments)) {
+            for (i = 0; i <= t.instruments.length - 1; i++) {
+                var newT = this[t.instruments[i]];
+                //Minimize the number of samples to load
+                if (this.minify === true || t.minify === true) {
+                    var minBy = 1;
+                    if (Object.keys(newT).length >= 17) {
+                        minBy = 2
+                    }
+                    if (Object.keys(newT).length >= 33) {
+                        minBy = 4
+                    }
+                    if (Object.keys(newT).length >= 49) {
+                        minBy = 6
+                    }
+
+                    var filtered = Object.keys(newT).filter(function (_, i) {
+                        return i % minBy != 0;
+                    })
+                    filtered.forEach(function (f) {
+                        delete newT[f]
+                    })
+
+                }
+
+
+
+
+                rt[t.instruments[i]] = new Tone.Sampler(
+                    newT, {
+                        baseUrl: t.baseUrl + t.instruments[i] + "/",
+                        onload: t.onload
+                    }
+
+                )
+            }
+
+            return rt
+
+            // if a single instrument name is passed...
+        } else {
+            newT = this[t.instruments];
+
+            //Minimize the number of samples to load
+            if (this.minify === true || t.minify === true) {
+                minBy = 1;
+                if (Object.keys(newT).length >= 17) {
+                    minBy = 2
+                }
+                if (Object.keys(newT).length >= 33) {
+                    minBy = 4
+                }
+                if (Object.keys(newT).length >= 49) {
+                    minBy = 6
+                }
+
+                filtered = Object.keys(newT).filter(function (_, i) {
+                    return i % minBy != 0;
+                })
+                filtered.forEach(function (f) {
+                    delete newT[f]
+                })
+            }
+
+
+
+
+            var s = new Tone.Sampler(
+                newT, {
+                    baseUrl: t.baseUrl + t.instruments + "/",
+                    onload: t.onload
+                }
+            )
+
+            return s
+        }
+
+    },
+
+    'harmonium': {
+        'C2': 'C2.[mp3|ogg]',
+        'C3': 'C3.[mp3|ogg]',
+        'C4': 'C4.[mp3|ogg]',
+        'D2': 'D2.[mp3|ogg]',
+        'D3': 'D3.[mp3|ogg]',
+        'D4': 'D4.[mp3|ogg]',
+        'D5': 'D5.[mp3|ogg]',
+        'E2': 'E2.[mp3|ogg]',
+        'E3': 'E3.[mp3|ogg]',
+        'E4': 'E4.[mp3|ogg]',
+        'F2': 'F2.[mp3|ogg]',
+        'F3': 'F3.[mp3|ogg]',
+        'F4': 'F4.[mp3|ogg]',
+        'G2': 'G2.[mp3|ogg]',
+        'G3': 'G3.[mp3|ogg]',
+        'G4': 'G4.[mp3|ogg]',
+        'A2': 'A2.[mp3|ogg]',
+        'A3': 'A3.[mp3|ogg]',
+        'A4': 'A4.[mp3|ogg]'
+    },
+
+    'piano': {
+        'A2': 'A2.[mp3|ogg]',
+        'A3': 'A3.[mp3|ogg]',
+        'A4': 'A4.[mp3|ogg]',
+        'B2': 'B2.[mp3|ogg]',
+        'B3': 'B3.[mp3|ogg]',
+        'B4': 'B4.[mp3|ogg]',
+        'C2': 'C2.[mp3|ogg]',
+        'C3': 'C3.[mp3|ogg]',
+        'C4': 'C4.[mp3|ogg]',
+        'C5': 'C5.[mp3|ogg]',
+        'D2': 'D2.[mp3|ogg]',
+        'D3': 'D3.[mp3|ogg]',
+        'D4': 'D4.[mp3|ogg]',
+        'E2': 'E2.[mp3|ogg]',
+        'E3': 'E3.[mp3|ogg]',
+        'E4': 'E4.[mp3|ogg]',
+        'F2': 'F2.[mp3|ogg]',
+        'F3': 'F3.[mp3|ogg]',
+        'F4': 'F4.[mp3|ogg]',
+        'G2': 'G2.[mp3|ogg]',
+        'G3': 'G3.[mp3|ogg]',
+        'G4': 'G4.[mp3|ogg]'
+    },
+
+    'cello': {
+        'A2': 'A2.[mp3|ogg]',
+        'A3': 'A3.[mp3|ogg]',
+        'A4': 'A4.[mp3|ogg]',
+        'B2': 'B2.[mp3|ogg]',
+        'B3': 'B3.[mp3|ogg]',
+        'B4': 'B4.[mp3|ogg]',
+        'C2': 'C2.[mp3|ogg]',
+        'C3': 'C3.[mp3|ogg]',
+        'C4': 'C4.[mp3|ogg]',
+        'D2': 'D2.[mp3|ogg]',
+        'D3': 'D3.[mp3|ogg]',
+        'D4': 'D4.[mp3|ogg]',
+        'E2': 'E2.[mp3|ogg]',
+        'E3': 'E3.[mp3|ogg]',
+        'E4': 'E4.[mp3|ogg]',
+        'F2': 'F2.[mp3|ogg]',
+        'F3': 'F3.[mp3|ogg]',
+        'F4': 'F4.[mp3|ogg]',
+        'G2': 'G2.[mp3|ogg]',
+        'G3': 'G3.[mp3|ogg]',
+        'G4': 'G4.[mp3|ogg]'
+    }
+}

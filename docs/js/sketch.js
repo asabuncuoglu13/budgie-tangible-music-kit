53a93d9a0a69888675b6fb6cb63b803137bb8793
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2019-10-27 15:10:07 +0300

    piano sound added

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -2,6 +2,7 @@ let index = 0;
 let inLoop = false;
 
 let song = [];
+let loopedSong = [];
 
 let trigger = 0;
 let autoplay = false;
@@ -11,8 +12,24 @@ let highFreq = true;
 let drum, beatbox;
 let osc = "sine";
 let recorder, soundFile;
+let loopTimes = 2;
+let currentBlock = "start";
 
 function preload() {
+    p_a3 = loadSound('sound/piano/a3.mp3');
+    p_a4 = loadSound('sound/piano/a4.mp3');
+    p_b3 = loadSound('sound/piano/b3.mp3');
+    p_b4 = loadSound('sound/piano/b4.mp3');
+    p_c3 = loadSound('sound/piano/c3.mp3');
+    p_c4 = loadSound('sound/piano/c4.mp3');
+    p_d3 = loadSound('sound/piano/d3.mp3');
+    p_d4 = loadSound('sound/piano/d4.mp3');
+    p_e3 = loadSound('sound/piano/e3.mp3');
+    p_e4 = loadSound('sound/piano/e4.mp3');
+    p_f3 = loadSound('sound/piano/f3.mp3');
+    p_f4 = loadSound('sound/piano/f4.mp3');
+    p_g3 = loadSound('sound/piano/g3.mp3');
+    p_g4 = loadSound('sound/piano/g4.mp3');
     beatbox = loadSound('sound/beatbox.mp3');
     drum = loadSound('sound/drum.mp3');
 }
@@ -36,6 +53,9 @@ function setup() {
 }
 
 function playSong() {
+    if (inLoop) {
+        createLoopedSong(loopTimes);
+    }
     if (!autoplay) {
         index = 0;
         autoplay = true;
@@ -62,6 +82,14 @@ function clearCode() {
 
 function startLoop() {
     inLoop = !inLoop;
+    currentBlock = "loop";
+}
+
+function createLoopedSong(num) {
+    loopedSong = [];
+    for (let i = 0; i < num; i++) {
+        loopedSong = loopedSong.concat(song);
+    }
 }
 
 function addNote(note, dur) {
@@ -123,6 +151,10 @@ function addNote(note, dur) {
     }
 }
 
+function addPiano() {
+    osc = "piano";
+}
+
 function setOssiloscope(type) {
     if (type === "sine") {
         osc = "sine";
@@ -133,6 +165,66 @@ function setOssiloscope(type) {
 
 
 function playNote(note, duration, soundType) {
+    if (soundType === "piano") {
+        switch (note) {
+            case 48:
+                p_c3.setVolume(1);
+                p_c3.play();
+                break;
+            case 50:
+                p_d3.setVolume(1);
+                p_d3.play();
+                break;
+            case 52:
+                p_e3.setVolume(1);
+                p_e3.play();
+                break;
+            case 53:
+                p_f3.setVolume(1);
+                p_f3.play();
+                break;
+            case 55:
+                p_g3.setVolume(1);
+                p_g3.play();
+                break;
+            case 57:
+                p_a3.setVolume(1);
+                p_a3.play();
+                break;
+            case 59:
+                p_b3.setVolume(1);
+                p_b3.play();
+                break;
+            case 60:
+                p_c4.setVolume(1);
+                p_c4.play();
+                break;
+            case 62:
+                p_d4.setVolume(1);
+                p_d4.play();
+                break;
+            case 64:
+                p_e4.setVolume(1);
+                p_e4.play();
+                break;
+            case 65:
+                p_f4.setVolume(1);
+                p_f4.play();
+                break;
+            case 67:
+                p_g4.setVolume(1);
+                p_g4.play();
+                break;
+            case 69:
+                p_a4.setVolume(1);
+                p_a4.play();
+                break;
+            case 71:
+                p_b4.setVolume(1);
+                p_b4.play();
+                break;
+        }
+    }
     if (soundType === "square") {
         oscT.freq(midiToFreq(note));
         oscT.fade(0.5, 0.2);
@@ -153,6 +245,9 @@ function playNote(note, duration, soundType) {
     }
 }
 
+function mousePressed() {
+    getAudioContext().resume();
+}
 
 function keyTyped() {
     if (key === 's') {
@@ -160,16 +255,32 @@ function keyTyped() {
     }
 }
 
+function keyPressed() {
+    if (currentBlock === "loop") {
+        if (keyCode === UP_ARROW) {
+            (loopTimes > 10) ? loopTimes = 10 : loopTimes++;
+        }
+        if (keyCode === DOWN_ARROW) {
+            (loopTimes < 2) ? loopTimes = 2 : loopTimes--;
+        }
+    }
+}
+
 function draw() {
     if (autoplay && millis() > trigger) {
         recorder.record(soundFile);
-        playNote(song[index].note, song[index].duration, song[index].soundType);
-        trigger = millis() + song[index].duration;
+        if (inLoop) {
+            playNote(loopedSong[index].note, loopedSong[index].duration, loopedSong[index].soundType);
+            trigger = millis() + loopedSong[index].duration;
+        } else {
+            playNote(song[index].note, song[index].duration, song[index].soundType);
+            trigger = millis() + song[index].duration;
+        }
         index++;
-        if (index >= song.length){
+        if (index >= song.length + 2) {
             recorder.stop();
         }
-    } else if (index >= song.length) {
+    } else if (inLoop ? index >= loopedSong.length : index >= song.length) {
         autoplay = false;
     }
     /*

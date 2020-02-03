2a99859870fcfd9ad8b70855a283eebdc659ccf6
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2020-02-03 22:15:51 +0300

    measure init

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -9,6 +9,7 @@ let synth;
 let eventIndex = 0;
 let beatsPerMinute = 120;
 let secondsPerBeat;
+let measure = 1;
 
 let trigger = 0;
 let autoplay = false;
@@ -70,8 +71,6 @@ function setup() {
     recorder = new p5.SoundRecorder();
     recorder.setInput();
     soundFile = new p5.SoundFile();
-
-    myPart = new p5.Part();
 }
 
 function noteToFreq(note) {
@@ -145,6 +144,10 @@ function changeFreq(val) {
 
 function changeBPM() {
     currentBlock = "bpm";
+}
+
+function changeMeasure() {
+    currentBlock = "measure";
 }
 
 function startSynth() {
@@ -354,51 +357,51 @@ function attackNote(note, duration, soundType) {
 
 function releaseNote(note, duration, soundType) {
     if (drumPat) {
-        drum.stop(1);
+        drum.stop(measure);
     }
     if (soundType === "piano") {
         switch (note) {
             case "C3":
-                p_c3.stop(1);
+                p_c3.fade(measure, measure);
                 break;
             case "D3":
-                p_d3.stop(1);
+                p_d3.stop(measure);
                 break;
             case "E3":
-                p_e3.stop(1);
+                p_e3.stop(measure);
                 break;
             case "F3":
-                p_f3.stop(1);
+                p_f3.stop(measure);
                 break;
             case "G3":
-                p_g3.stop(1);
+                p_g3.stop(measure);
                 break;
             case "A3":
-                p_a3.stop(1);
+                p_a3.stop(measure);
                 break;
             case "B3":
-                p_b3.stop(1);
+                p_b3.stop(measure);
                 break;
             case "C4":
-                p_c4.stop(1);
+                p_c4.fade(measure, measure);
                 break;
             case "D4":
-                p_d4.stop(1);
+                p_d4.stop(measure);
                 break;
             case "E4":
-                p_e4.stop(1);
+                p_e4.stop(measure);
                 break;
             case "F4":
-                p_f4.stop(1);
+                p_f4.stop(measure);
                 break;
             case "G4":
-                p_g4.stop(1);
+                p_g4.stop(measure);
                 break;
             case "A4":
-                p_a4.stop(1);
+                p_a4.stop(measure);
                 break;
             case "B4":
-                p_b4.stop(1);
+                p_b4.stop(measure);
                 break;
             default:
                 break;
@@ -407,46 +410,46 @@ function releaseNote(note, duration, soundType) {
     if (soundType === "guitar") {
         switch (note) {
             case "C3":
-                g_c3.stop(1);
+                g_c3.stop(measure);
                 break;
             case "D3":
-                g_d3.stop(1);
+                g_d3.stop(measure);
                 break;
             case "E3":
-                g_e3.stop(1);
+                g_e3.stop(measure);
                 break;
             case "F3":
-                g_f3.stop(1);
+                g_f3.stop(measure);
                 break;
             case "G3":
-                g_g3.stop(1);
+                g_g3.stop(measure);
                 break;
             case "A3":
-                g_a3.stop(1);
+                g_a3.stop(measure);
                 break;
             case "B3":
-                g_b3.stop(1);
+                g_b3.stop(measure);
                 break;
             case "C4":
-                g_c4.stop(1);
+                g_c4.stop(measure);
                 break;
             case "D4":
-                g_d4.stop(1);
+                g_d4.stop(measure);
                 break;
             case "E4":
-                g_e4.stop(1);
+                g_e4.stop(measure);
                 break;
             case "F4":
-                g_f4.stop(1);
+                g_f4.stop(measure);
                 break;
             case "G4":
-                g_g4.stop(1);
+                g_g4.stop(measure);
                 break;
             case "A4":
-                g_a4.stop(1);
+                g_a4.stop(measure);
                 break;
             case "B4":
-                g_b4.stop(1);
+                g_b4.stop(measure);
                 break;
             default:
                 break;
@@ -476,28 +479,28 @@ function touchStarted() {
 function keyTyped() {
     switch (key) {
         case 'a' || 'A':
-            addNote('A', 1);
+            addNote('A', measure);
             break;
         case 'b' || 'B':
-            addNote('B', 1);
+            addNote('B', measure);
             break;
         case 'c' || 'C':
-            addNote('C', 1);
+            addNote('C', measure);
             break;
         case 'd' || 'D':
-            addNote('D', 1);
+            addNote('D', measure);
             break;
         case 'e' || 'E':
-            addNote('E', 1);
+            addNote('E', measure);
             break;
         case 'f' || 'F':
-            addNote('F', 1);
+            addNote('F', measure);
             break;
         case 'g' || 'G':
-            addNote('G', 1);
+            addNote('G', measure);
             break;
         case 'n' || 'N':
-            addNote('N', 1);
+            addNote('N', measure);
             break;
         case '1':
             startSynth();
@@ -541,6 +544,18 @@ function saveFile() {
     save(soundFile, 'myAlgorithmicMusic.wav');
 }
 
+function increaseMeasure() {
+    if (measure === 1) measure = 0.5;
+    if (measure === 2) measure = 1;
+    if (measure === 4) measure = 2;
+}
+
+function decreaseMeasure() {
+    if (measure === 2) measure = 4;
+    if (measure === 1) measure = 2;
+    if (measure === 0.5) measure = 1;
+}
+
 function increaseBPM() {
     (beatsPerMinute > 180) ? beatsPerMinute = 180 : beatsPerMinute+=5;
 }
@@ -575,6 +590,15 @@ function keyPressed() {
             decreaseBPM();
         }
         document.getElementById("bpmTimes").innerHTML = beatsPerMinute;
+    }
+    if (currentBlock === "measure") {
+        if (keyCode === UP_ARROW) {
+            increaseMeasure();
+        }
+        if (keyCode === DOWN_ARROW) {
+            decreaseMeasure();
+        }
+        document.getElementById("measureTimes").innerHTML = 4/measure;
     }
 }
 
